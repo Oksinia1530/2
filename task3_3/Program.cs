032fc1d0368d78d2f44a6f29becedda46682be74
«Oksana <averinaoy@gmail.com>
2023-02-12 00:14:34 +0500

    task3_3

---
+++ task3_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Clear();
+Console.WriteLine("Enter number N");
+int N = int.Parse(Console.ReadLine()!);
+int cube = N*N*N;
+if (cube % 2== 0)
+{
+    Console.WriteLine(cube);
+}
+else
+{
+    Console.WriteLine("Enter nuw number");
+}
