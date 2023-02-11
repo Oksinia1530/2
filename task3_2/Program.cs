7b3df15359871bf7e4396ef838dff8f176764ae2
«Oksana <averinaoy@gmail.com>
2023-02-12 00:01:31 +0500

    task3_2

---
+++ task3_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.Clear();
+Console.WriteLine("Enter number N");
+int N = int.Parse(Console.ReadLine()!);
+int result = 0;
+for (int i = 1; i <= N; i++)
+{
+result*=i*i;
+
+}
+Console.WriteLine(result);
