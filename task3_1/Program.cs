cd0e7db6ad9bc3a9f43779c8ea27e847b03a0a29
«Oksana <averinaoy@gmail.com>
2023-02-11 23:45:37 +0500

    task3_1

---
+++ task3_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.Clear();
+Console.WriteLine("Enter number ");
+int number = int.Parse(Console.ReadLine()!);
+int result = 0;
+
+while (number>0)
+{
+    result=result + number%10;
+    number= number/10;
+}
+
+Console.WriteLine(result);
